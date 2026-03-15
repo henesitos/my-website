a48d516b6a1bb8bcf01cc14352fc1497f24f8296
henesitos <henesitos@gmail.com>
2026-03-15 08:46:49 +0800

    added more fun fact

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 var funFactBtn = document.getElementById("funFactBtn");
 var funFactText = document.getElementById("funFactText");
 
+var facts = [
+    "I have a playlist with over 500 songs on Spotify!",
+    "I once binge-watched an entire anime series in one weekend.",
+    "My favorite travel memory is waking up to a mountain view.",
+    "I've been gaming since I was 5 years old.",
+    "I can listen to the same song on repeat for hours and not get tired of it."
+];
+
+var factIndex = 0;
+
 funFactBtn.addEventListener("click", function() {
-    funFactText.textContent = "Fun Fact: I have a playlist with over 500 songs on Spotify!";
+    funFactText.textContent = "Fun Fact: " + facts[factIndex];
+    factIndex = factIndex + 1;
+    if (factIndex >= facts.length) {
+        factIndex = 0;
+    }
 });
